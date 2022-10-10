8c3d7560829a08b7e5e6682adc7e1f717272c9fa
Abhishek Kumar Singh <abhisheksingh1911a5@gmail.com>
2022-10-10 08:06:28 +0530

    SIGTERM

--- server.js
+++ server.js
@@ -35,3 +35,10 @@ process.on('unhandledRejection', err => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.error('SIGTERM RECIEVED! 💥 Shutting down...');
+  server.close(() => {
+    console.log('Process Terminated');
+  });
+});
